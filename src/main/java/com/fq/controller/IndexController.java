4fe395a8db188d294f99990b56802a73c57ccb54
fangqianseu <fangqiandndx@gmail.com>
2019-04-11 16:36:34 +0800

    增加 首页 的问题广场逻辑

--- src/main/java/com/fq/controller/IndexController.java
+++ src/main/java/com/fq/controller/IndexController.java
@@ -3,15 +3,41 @@ Date: 04/11,2019, 09:26
 */
 package com.fq.controller;
 
+import com.fq.model.Question;
+import com.fq.model.SessionHolder;
+import com.fq.model.ViewObject;
+import com.fq.service.QuestionService;
+import com.fq.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private SessionHolder sessionHolder;
 
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        List<Question> latestQuestions = questionService.getLatestQuestions(0, 0, 20);
+        List<ViewObject> questions = new ArrayList<>();
+        for (Question q : latestQuestions) {
+            ViewObject vo = new ViewObject();
+            vo.set("question", q);
+            vo.set("user", userService.getUserById(q.getUserId()));
+            questions.add(vo);
+        }
+        model.addAttribute("vos", questions);
+
         return "index";
     }
 }
